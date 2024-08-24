8c324e491b3416034387f0a45ce06b27773f32c9
Tushar <modematrix3@gmail.com>
2024-08-24 09:29:58 +0530

    minor update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Button() {
   return (
     <div className='flex gap-5'>
-        <button className='h-16 w-24 my-9 bg-red-800 text-white text-xl rounded-md shadow-md'><a href="https://forms.gle/cskiVETw4efrCBFU9">Register</a></button>
-        <button className='h-16 w-24 my-9 bg-red-800 text-white text-xl rounded-md shadow-md'><a href="">Visit SIH</a></button>
+        <button className='h-16 w-24 my-9 bg-red-800 text-white text-xl rounded-md shadow-md hover:bg-red-700'><a href="https://forms.gle/cskiVETw4efrCBFU9">Register</a></button>
+        <button className='h-16 w-24 my-9 bg-red-800 text-white text-xl rounded-md shadow-md hover:bg-red-700'><a href="https://sih.gov.in">Visit SIH</a></button>
 
     </div>
   )
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -9,17 +9,20 @@ export default function Title() {
   return (
     <>
       <div className=" flex bg-red-800 justify-around p-1 roboto items-center  lg:gap-5 shadow-md "> 
+          <a href="https://keshav.du.ac.in/">
         <div className=" flex justify-center items-center  lg:ml-5">
           <div className=" h-28 w-32">
 
             <img className="object-fill " src={logo} alt="" />
           </div>
         <div className=" scale-90 text-white flex flex-col justify-center items-center">
+
             <h1 className="text-xl font-bold roboto-slab-b ">KESHAV MAHAVIDYALAYA</h1>
             <h2>University of Delhi</h2>
             <h2>NAAC Accredited 'A' Grade Cycle 2</h2>
         </div>
         </div>
+          </a>
 
         <div className="relative lg:flex lg:visible gap-10 hidden bg-red-800 justify-center items-center">
           <img className="h-24" src={independence} alt="" />
