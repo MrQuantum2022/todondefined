08df223970487b046facdbf73767e6c6dfd195fb
Tushar <modematrix3@gmail.com>
2024-08-24 16:47:53 +0530

    adjustment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,18 +4,18 @@ export default function Footer() {
   return (
     <div className='w-screen py-5 bg-amber-600 flex justify-center items-center'>
         <div className='w-auto lg:w-10/12'>
-        <div className=' text-white font-semibold text-3xl mb-5'>Student Coordinators</div>
+        <div className=' text-white font-semibold text-2xl mb-5'>Student Coordinators</div>
         <div className='text-white flex flex-col lg:flex-row gap-5'>
-            <li className='flex flex-col text-xl'> Tushar-3rd year
+            <li className='flex flex-col'> Tushar-3rd year
                 <div>9311706455</div>
             </li>
-            <li className='flex flex-col text-xl'> Nancy-3rd year
+            <li className='flex flex-col'> Nancy-3rd year
                 <div>9260938565</div>
             </li>
-            <li className='flex flex-col text-xl'> Adarsh Maurya-2nd year
+            <li className='flex flex-col'> Adarsh Maurya-2nd year
                 <div>7275156652</div>
             </li>
-            <li className='flex flex-col text-xl'> Anjali-2rd year
+            <li className='flex flex-col'> Anjali-2rd year
                 <div>9334286212</div>
             </li>
         </div>
