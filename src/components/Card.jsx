6c61590ff4ea2f195e391423ba88c116342019f2
Tushar <modematrix3@gmail.com>
2024-08-26 21:28:36 +0530

    banner updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import banner from '../assets/banner1.png'
+import banner from '../assets/sihbanner.jpg'
 import { SlCalender } from "react-icons/sl";
 import { FaCommentDots } from "react-icons/fa";
 import Button from './Button';
